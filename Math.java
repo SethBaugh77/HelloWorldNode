fa0e1e748088b44a46594a8a44649ed50dde35ad
SethBaugh77 <eif8e8di@gmail.com>
2022-06-06 10:17:02 -0400

    commit Math.java file

---
+++ Math.java
@@ -0,0 +1,33 @@
+
+
+
+
+class Math{
+
+
+
+public static void main(String args[])
+{
+    int a = 5;
+    int b = 6;
+
+    int c = a+b;
+
+    System.out.println("Hey, a = " + a);
+    System.out.println("Hey, b = " + b);
+    System.out.println("Hey, c = " + c);
+
+
+    System.out.println("PLZ WORK!!!!");
+
+}
+
+
+
+
+
+
+
+
+
+}
